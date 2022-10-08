bb3db76aa2dffe6d8fe2d0125f3a07a30a3dac41
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-10-08 11:57:32 -0400

    Basic styling to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function Navbar() {
 	return (
-		<div className="container h-1/6">
-			<h1 className="text-4xl font-bold text-blue">Secret Salary Navbar</h1>
+		<div className="container flex h-4 items-center justify-between">
+			<h1 className="text-4xl font-bold text-blue">Secret Salary</h1>
+			<div className="flex list-none flex-row space-x-4 text-white">
+				<li>Salary Possibilities</li>
+				<li>Spill The Salary Tea</li>
+			</div>
 		</div>
 	);
 }
