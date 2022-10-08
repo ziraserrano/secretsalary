d949d51ebe0294a1d8137e17fcf05e34db7ce453
Zira Serrano <ziraserrano@gmail.com>
2022-10-08 16:28:45 -0400

    Fix form/input fields

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,18 +2,21 @@ import React from "react";
 
 export default function Table() {
 	return (
-		<div className="container flex h-max items-center justify-center">
+		<div className="container">
 			<form class="flex gap-1">
-				<input placeholder="Company"></input>
-				<input placeholder="Title"></input>
-				<input placeholder="Location"></input>
-				<input placeholder="Location"></input>
-				<input placeholder="Experience"></input>
-				<input placeholder="Base"></input>
-				<input placeholder="Bonus"></input>
-				<input placeholder="Total"></input>
+				<div>
+					<input type="text" id="company" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Company" required></input>
+				</div>
+				<div>
+					<input type="text" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title" required></input>
+				</div>
+				<div>
+					<input type="text" id="location" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Location" required></input>
+				</div>
 			</form>
-			<h1 className="text-3xl font-bold text-blue">Table of Secret Salaries</h1>
+			<div className="container flex h-max items-center justify-center">
+				<h1 className="text-3xl font-bold text-blue">Table of Secret Salaries</h1>
+			</div>
 		</div>
 	);
 }
