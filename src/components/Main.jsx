ed6ab52b4ed84879605ee011167980a19762ec83
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-10-08 11:46:04 -0400

    Add basic styling to Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 export default function Main() {
 	return (
-		<div className="container h-3/5">
-			<h1 className="text-4xl font-bold text-blue">Secret Salary Main</h1>
+		<div className="container flex h-3/5 flex-col items-center justify-center">
+			<h1 className="text-6xl font-bold text-blue">
+				Anonymous Salary Transparency
+			</h1>
+			<p className="py-5 text-2xl text-white">
+				Let's make salary transparency more accessible. Start by sharing yours.
+			</p>
 		</div>
 	);
 }
