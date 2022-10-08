d156af212fcf42831ed37533af60139ab5f94d44
Zira Serrano <ziraserrano@gmail.com>
2022-10-08 16:05:44 -0400

    Add spacing to theme in tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,6 +12,14 @@ module.exports = {
 			black: "#191e28ff",
 			white: "#e5e5e5",
 		},
+		spacing: {
+			'1': '8px',
+			'2': '12px',
+			'3': '16px',
+			'4': '24px',
+			'5': '32px',
+			'6': '48px',
+		  },
 	},
 	plugins: [require("prettier-plugin-tailwindcss")],
 };
