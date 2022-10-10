2784bf5ed7745476b99646205c343e6ab99f1d6b
Zira Serrano <ziraserrano@gmail.com>
2022-10-10 14:32:38 -0400

    Change Coming Soon banner

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Table from "./components/Table";
 function App() {
 	return (
 		<div className="absolute h-screen w-screen bg-gray">
-			<body className="absolute h-screen w-screen bg-seeThrough text-white flex justify-center items-center text-6xl">Coming soon!</body>
+			<body className="absolute h-screen w-screen bg-seeThrough text-white flex justify-center items-center text-6xl">Coming soon...</body>
 				<div className="h-screen bg-black p-4">
 					<Navbar />
 					<Main />
